9c67b4b000af3921602f935f2f39c35a0a27bb38
Allan Adan <allanadan1999@gmail.com>
2021-03-10 03:52:27 +0800

    design1 ready for responsiveness

---
+++ styles.js
@@ -0,0 +1,33 @@
+// Circle Hover at Section 1
+function noHover() {
+    document.getElementById("lighter-circle-hover").style.display = "none";
+    document.getElementById("light-circle-hover").style.display = "none";
+    document.getElementById("dark-circle-hover").style.display = "none";
+    document.getElementById("no-circle-hover").style.display = "inline";
+}
+
+function lighter() {
+    document.getElementById("lighter-circle-hover").style.display = "inline";
+    document.getElementById("no-circle-hover").style.display = "none";
+}
+
+function light() {
+    document.getElementById("light-circle-hover").style.display = "inline";
+    document.getElementById("no-circle-hover").style.display = "none";
+}
+
+function dark() {
+    document.getElementById("dark-circle-hover").style.display = "inline";
+    document.getElementById("no-circle-hover").style.display = "none";
+}
+
+// Accordion at Section 2
+
+function toggleAccordion(id) {
+    var x = document.getElementById(id);
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
